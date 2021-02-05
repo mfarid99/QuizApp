9533b0a9e655d8b97ba012bcbc9ac37646876854
moe <moe.faridd@gmail.com>
2021-02-04 23:03:47 -0500

    Trivia done

--- js/app.js
+++ js/app.js
@@ -61,3 +61,57 @@ const triviaData = [
     correct: 'b',
   },
 ]
+
+const trivia = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+const loadQuiz = () => {
+  deselectAnswers()
+  const currentQuizData = triviaData[currentQuiz]
+  questionEl.innerText = currentQuizData.question
+  a_text.innerText = currentQuizData.a
+  b_text.innerText = currentQuizData.b
+  c_text.innerText = currentQuizData.c
+  d_text.innerText = currentQuizData.d
+}
+
+const deselectAnswers = () => {
+  answerEls.forEach((answerEl) => (answerEl.checked = false))
+}
+
+const getSelected = () => {
+  let answer
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id
+    }
+  })
+  return answer
+}
+
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected()
+  if (answer) {
+    if (answer === triviaData[currentQuiz].correct) {
+      score++
+    }
+    currentQuiz++
+
+    if (currentQuiz < triviaData.length) {
+      loadQuiz()
+    } else {
+      quiz.innerHTML = `<h2>You scored ${score} Out Of ${triviaData.length}, Click Reload To Play Again!</h2> <button onclick="location.reload()">Reload</button>`
+    }
+  }
+})
+
+loadQuiz()
