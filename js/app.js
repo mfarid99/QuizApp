3767c1f6c74ec52b05598236e27d99a6e77be146
moe <moe.faridd@gmail.com>
2021-02-04 21:53:10 -0500

    questions added

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const triviaData = [
+  {
+    question: 'Who was POTUS 42?',
+    a: 'Ronald Regan',
+    b: 'Bill Clinton',
+    c: 'George H. W. Bush',
+    d: 'Barack Obama',
+    correct: 'b',
+  },
+  {
+    question: 'How many letters are in the alphabet?',
+    a: '26',
+    b: '27',
+    c: '22',
+    d: '24',
+    correct: 'a',
+  },
+
+  {
+    question: 'How many kilometers in a mile?',
+    a: '2',
+    b: '2.1',
+    c: '1.84',
+    d: '1.6',
+    correct: 'd',
+  },
+
+  {
+    question: 'How many mile(s) in a kilometer?',
+    a: '0.62',
+    b: '0.59',
+    c: '1.2',
+    d: '1.6',
+    correct: 'a',
+  },
+
+  {
+    question: 'React js is...',
+    a:
+      'Is a platform and framework for building single-page client applications using HTML and TypeScript',
+    b: 'A JavaScript library for building user interfaces',
+    c: 'An After Effects plugin',
+    d: 'A 3D animation program',
+    correct: 'b',
+  },
+
+  {
+    question: 'How many teeth can a shark have in its lifetime?',
+    a: '16',
+    b: '1820',
+    c: '29880',
+    d: '240',
+    correct: 'c',
+  },
+  {
+    question: 'Which animal can regenerate limbs and major organs?',
+    a: 'Turtle',
+    b: 'Axolotl',
+    c: 'Octopus',
+    d: 'Fruit Fly',
+    correct: 'b',
+  },
+]
